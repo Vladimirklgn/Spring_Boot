2dda5de331b536e349c2b42e337554a9f325f6e1
Vladimirklgn <Vladimirklgn@github.com>
2024-11-26 22:20:54 +0500

    first commit

---
+++ src/main/java/org/example/service/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package org.example.service;
+
+import org.example.entity.User;
+import org.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    @Override
+    public void addUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Transactional
+    @Override
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    @Override
+    public User getUser(Long id) {
+        return userRepository.getOne(id);
+    }
+
+    @Transactional
+    @Override
+    public void updateUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Transactional
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
